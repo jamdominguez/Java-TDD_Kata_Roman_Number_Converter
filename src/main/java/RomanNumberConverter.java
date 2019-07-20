f1b636abad066170a9cc1886c62da102283965db
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-07-20 22:24:32 +0200

    refactor

--- src/main/java/RomanNumberConverter.java
+++ src/main/java/RomanNumberConverter.java
@@ -2,9 +2,10 @@ public class RomanNumberConverter {
     public static String toRoman(int i) {
         StringBuilder result = new StringBuilder();
 
-        if (i == 7) return "VII";
-        if (i == 6) return "VI";
-        if (i == 5) return "V";
+        if (i >= 5) {
+            result.append("V");
+            i -= 5;
+        }
         if (i == 4) return "IV";
         while(i-- > 0){
             result.append("I");
