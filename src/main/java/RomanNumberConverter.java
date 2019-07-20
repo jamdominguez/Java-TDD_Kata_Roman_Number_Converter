4099a3ad3909e3295fa99f81b17e33129c7dc3c1
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-07-20 23:58:09 +0200

    test passed to 100

--- src/main/java/RomanNumberConverter.java
+++ src/main/java/RomanNumberConverter.java
@@ -1,19 +1,57 @@
+import java.util.*;
+
 public class RomanNumberConverter {
+
+    public static Map SYMBOLS;
+
+    static {
+        SYMBOLS = new HashMap();
+        SYMBOLS.put(1, "I");
+        SYMBOLS.put(5, "V");
+        SYMBOLS.put(10, "X");
+        SYMBOLS.put(50, "L");
+        SYMBOLS.put(100, "C");
+        SYMBOLS.put(500, "D");
+        SYMBOLS.put(1000, "M");
+    }
+
     public static String toRoman(int i) {
         StringBuilder result = new StringBuilder();
 
-        if (i >= 10) {
-            result.append("X");
+        if (i == 100) {
+            result.append(SYMBOLS.get(100));
+            return result.toString();
+        }
+        if (i >= 90) {
+            result.append(SYMBOLS.get(10)).append(SYMBOLS.get(100));
+            i -= 90;
+        }
+        if (i >= 50) {
+            result.append(SYMBOLS.get(50));
+            i -= 50;
+        }
+        if (i >= 40) {
+            result.append(SYMBOLS.get(10)).append(SYMBOLS.get(50));
+            i -=40;
+        }
+        while (i >= 10) {
+            result.append(SYMBOLS.get(10));
             i -= 10;
         }
-        if (i == 9) return "IX";
+        if (i == 9) {
+            result.append(SYMBOLS.get(1)).append(SYMBOLS.get(10));
+            return result.toString();
+        }
         if (i >= 5) {
-            result.append("V");
+            result.append(SYMBOLS.get(5));
             i -= 5;
         }
-        if (i == 4) return "IV";
-        while(i-- > 0){
-            result.append("I");
+        if (i == 4) {
+            result.append(SYMBOLS.get(1)).append(SYMBOLS.get(5));
+            return result.toString();
+        }
+        while (i-- > 0) {
+            result.append(SYMBOLS.get(1));
         }
 
         return result.toString();
--- src/test/java/RomanNumberConverterTest.java
+++ src/test/java/RomanNumberConverterTest.java
@@ -52,4 +52,64 @@ public class RomanNumberConverterTest {
     public void tenReturnX(){
         Assert.assertEquals("X", RomanNumberConverter.toRoman(10));
     }
+
+    @Test
+    public void elevenReturnXI(){
+        Assert.assertEquals("XI", RomanNumberConverter.toRoman(11));
+    }
+
+    @Test
+    public void twelveReturnXII(){
+        Assert.assertEquals("XII", RomanNumberConverter.toRoman(12));
+    }
+
+    @Test
+    public void thirteenReturnXIII(){
+        Assert.assertEquals("XIII", RomanNumberConverter.toRoman(13));
+    }
+
+    @Test
+    public void fourteenReturnXIV(){
+        Assert.assertEquals("XIV", RomanNumberConverter.toRoman(14));
+    }
+
+    @Test
+    public void criticalNumbersFrom15To100() {
+        Assert.assertEquals("XV", RomanNumberConverter.toRoman(15));
+        Assert.assertEquals("XX", RomanNumberConverter.toRoman(20));
+        Assert.assertEquals("XXIV", RomanNumberConverter.toRoman(24));
+        Assert.assertEquals("XXV", RomanNumberConverter.toRoman(25));
+        Assert.assertEquals("XXX", RomanNumberConverter.toRoman(30));
+        Assert.assertEquals("XXXV", RomanNumberConverter.toRoman(35));
+        Assert.assertEquals("XL", RomanNumberConverter.toRoman(40));
+        Assert.assertEquals("XLIV", RomanNumberConverter.toRoman(44));
+        Assert.assertEquals("XLV", RomanNumberConverter.toRoman(45));
+        Assert.assertEquals("XLIX", RomanNumberConverter.toRoman(49));
+        Assert.assertEquals("L", RomanNumberConverter.toRoman(50));
+        Assert.assertEquals("LXXX", RomanNumberConverter.toRoman(80));
+        Assert.assertEquals("LXXXV", RomanNumberConverter.toRoman(85));
+        Assert.assertEquals("XC", RomanNumberConverter.toRoman(90));
+        Assert.assertEquals("XCV", RomanNumberConverter.toRoman(95));
+        Assert.assertEquals("XCVIII", RomanNumberConverter.toRoman(98));
+        Assert.assertEquals("XCIX", RomanNumberConverter.toRoman(99));
+        Assert.assertEquals("C", RomanNumberConverter.toRoman(100));
+    }
+
+    @Test
+    public void criticalNumbersFrom104To999(){
+        Assert.assertEquals("CIV", RomanNumberConverter.toRoman(104));
+        Assert.assertEquals("CCCXCIX", RomanNumberConverter.toRoman(399));
+        Assert.assertEquals("CDXXIV", RomanNumberConverter.toRoman(424));
+        Assert.assertEquals("CDXLIV", RomanNumberConverter.toRoman(444));
+        Assert.assertEquals("CDXLIX", RomanNumberConverter.toRoman(449));
+        Assert.assertEquals("CDXCIV", RomanNumberConverter.toRoman(494));
+        Assert.assertEquals("CDXCIX", RomanNumberConverter.toRoman(499));
+        Assert.assertEquals("D", RomanNumberConverter.toRoman(500));
+        Assert.assertEquals("DCXCIX", RomanNumberConverter.toRoman(699));
+        Assert.assertEquals("DCCXXIV", RomanNumberConverter.toRoman(724));
+        Assert.assertEquals("DCCCLIV", RomanNumberConverter.toRoman(854));
+        Assert.assertEquals("CMXCIV", RomanNumberConverter.toRoman(994));
+        Assert.assertEquals("CMXCIX", RomanNumberConverter.toRoman(999));
+        Assert.assertEquals("M", RomanNumberConverter.toRoman(1000));
+    }
 }
